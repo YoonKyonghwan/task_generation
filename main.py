7cb81f0a3241af0ff7d56baaa7eb4399edb8abd0
Yoon Kyonghwan <ykw6644@gmail.com>
2024-04-05 15:28:50 +0900

    add arguments to run

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import json
 import math
-import random
+import argparse
 
 from taskset_generation import generate_random_periods_discrete, generate_uunifastdiscard, generate_tasksets
 
-def generate_available_period_list(min_period: int, max_period: int, sample_list: list = [1,2,5]) -> list:
+def generate_available_period_list(min_period: int, max_period: int, sample_list: list = [1,2,3,5]) -> list:
     multiplicator = 1
     period_list = []
     
@@ -68,6 +68,7 @@ def generate_json_file(testset_list: list, dirname: str, filename_prefix):
             task_info_dict['bodyTime'] = exec_time
             task_info_dict['writeTime'] = 0.0
             task_info_dict['nice'] = set_nice_value_by_deadline(period=period, min_period=min_period_in_testset)
+            #task_info_dict['nice'] = 0
             task_info_dict['period'] = period
             task_info_dict['initialPriority'] = -1
             task_info_dict['index'] = idx
@@ -79,25 +80,32 @@ def generate_json_file(testset_list: list, dirname: str, filename_prefix):
             json.dump(json_object, f, indent=2)
             
             
+            
+if __name__ == "__main__":
+    # num_cores = [1] # not used
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--generated_files_save_dir", type=str, default="generated_tasksets", help="Directory to save generated tasksets")
+    parser.add_argument("-n", "--num_tasksets", type=int, default=3, help="Number of tasks in a taskset")
+    parser.add_argument("--min_period", type=int, default=30000, help="Minimum period of tasks(us)")
+    parser.add_argument("--max_period", type=int, default=1000000, help="Maximum period of tasks(us)")
+    args = parser.parse_args()
+    generated_files_save_dir= args.generated_files_save_dir
+    num_sets = args.num_tasksets
+    min_period = args.min_period
+    max_period = args.max_period
+    num_tasks = [3, 6, 9, 12]
+    utilizations = [0.4, 0.6, 0.8]
 
-# num_cores = [1] # not used
-generated_files_save_dir="../generated_taskset_new2"
-num_tasks = [3, 6, 9, 12]
-utilizations = [0.2, 0.4, 0.6]
-num_sets = 50
-min_period = 10000
-max_period = 1000000
+    available_period_list = generate_available_period_list(min_period, max_period)
 
-available_period_list = generate_available_period_list(min_period, max_period)
-
-for num_task in num_tasks:
-    for utilization in utilizations:
-        period_list = generate_random_periods_discrete(num_periods=num_task, num_sets=num_sets, available_periods=available_period_list)
-        utilization_list = generate_uunifastdiscard(nsets=num_sets, u=utilization, n=num_task)
-        full_set = generate_tasksets(periods=period_list, utilizations=utilization_list)
-        
-        save_directory = os.path.join(generated_files_save_dir, "1cores", str(num_task)+"tasks", str(utilization)+"utilization")
-        os.makedirs(save_directory, exist_ok=True)
-        filename_prefix = "1cores_" + str(num_task)+"tasks_" + str(utilization)+"utilization_"
-        generate_json_file(full_set, save_directory, filename_prefix)
-        
+    for num_task in num_tasks:
+        for utilization in utilizations:
+            period_list = generate_random_periods_discrete(num_periods=num_task, num_sets=num_sets, available_periods=available_period_list)
+            utilization_list = generate_uunifastdiscard(nsets=num_sets, u=utilization, n=num_task)
+            full_set = generate_tasksets(periods=period_list, utilizations=utilization_list)
+            
+            save_directory = os.path.join(generated_files_save_dir, "1cores", str(num_task)+"tasks", str(utilization)+"utilization")
+            os.makedirs(save_directory, exist_ok=True)
+            filename_prefix = "1cores_" + str(num_task)+"tasks_" + str(utilization)+"utilization_"
+            generate_json_file(full_set, save_directory, filename_prefix)
+            
